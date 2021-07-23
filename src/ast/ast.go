b612bd4fb57899b9af5fc94e74a6ad2a4bd6fd68
schnell3526 <schnelldickheiter@gmail.com>
2021-07-23 05:42:59 +0000

    2.4 implement let statement parser

---
+++ src/ast/ast.go
@@ -0,0 +1,59 @@
+package ast
+
+import "github.com/schnell3526/writing_an_interpreter_in_go/src/token"
+
+/*
+DEFINE INTERFACE
+	Node
+	├── Statement
+	└── Expression
+*/
+
+// The base Node interface
+type Node interface {
+	TokenLiteral() string
+}
+
+// All statement nodes implement this
+type Statement interface {
+	Node
+	statementNode()
+}
+
+// All expression nodes implement this
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// root node
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+// Statements
+type LetStatement struct {
+	Token token.Token // token.LET
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+// Expression
+type Identifier struct {
+	Token token.Token // token.IDENT
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
